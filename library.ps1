c7941e789e58f9fa182e0eb6f174b8122190b187
y-the-alien <52802934+y-the-alien@users.noreply.github.com>
2021-10-06 19:46:33 -0300

    Novos ajustes

--- library.ps1
+++ library.ps1
@@ -2,10 +2,7 @@ function ExtractAudio {
 
     param($in,$out)
 
-    Write-Host "------------------------"
-    Write-Host "Extraindo audio"
-    Write-Host "Entrada: $($in)"
-    Write-Host "Saida: $($out+ '.wav')"
+    Write-Host "Extraindo audio..."
 
     if(Test-Path -Path $in -PathType Leaf){
         # $dir = (Get-Item $in).DirectoryName
@@ -13,28 +10,25 @@ function ExtractAudio {
         # $file = $dir + '\' + (Get-Item $in).Basename
         # $ext = (Get-Item $in).Extension
         
-        ffmpeg -i $in -vn -ac 1 ($out + '.wav') -y
+        ffmpeg -i $in -vn -ac 1 $out'.wav' -y
     }
 }
 
 function SpikeRemove {
     
     param($in,$out)
-    
-        Write-Host "------------------------"
-        Write-Host "Extraindo picos"
-        Write-Host "Entrada: $($in)"
-        Write-Host "Saida: $($out)"
+
+    Write-Host "Removendo picos..."       
     
     if(Test-Path -Path $in -PathType Leaf){
+        
         $file = (Get-Item $in).DirectoryName + '\' + (Get-Item $in).Basename
         $ext = (Get-Item $in).Extension
-    
-    
+            
         #Cria um arquivo temporário para análise dos picos utilizando lowpass
         $lowpass = 20
         ffmpeg -i $in -af "lowpass=f=$($lowpass)" $file'_lowpass'$ext -y
-
+        
         #Análise da sonoridade do arquivo lowpass
         ffmpeg -i $file'_lowpass'$ext -hide_banner -nostats -af 'loudnorm=I=-16:TP=-1.5:LRA=11:print_format=summary' -f null - 2>$file'_lowpass.txt' | Format -y
 
@@ -47,25 +41,26 @@ function SpikeRemove {
         #Listando as linhas do arquivo
         #Aplicando substituições para leitura do arquivo de texto
         $Extension=$Text | ForEach-Object{$_ -replace ":","|"} `
-                    | ForEach-Object{$_ -replace "dBTP","|"} `
-                    | ForEach-Object{$_ -replace "\s+",""} `
-                    | Where-Object { $_ -notmatch "size"} `
-                    | Where-Object { $_ -notmatch "Summary"} `
-
+        | ForEach-Object{$_ -replace "dBTP","|"} `
+        | ForEach-Object{$_ -replace "\s+",""} `
+        | Where-Object { $_ -notmatch "size"} `
+        | Where-Object { $_ -notmatch "Summary"} `
+        
         foreach ($element in $Extension) {
+        
             $item = $element
             $item=$item.split('|');
-    
+                
             if($item.count -eq 3 -and $item[0] -eq "InputTruePeak") {
                 $InputTruePeak = $item[1]
             }
         }
-
+            
         #Amplificando o sinal da saída para detectação dos picos
         $Boost = 15;
         $InputTruePeak = $Boost - $InputTruePeak
         ffmpeg -i $file'_lowpass'$ext -af "volume=$($InputTruePeak)dB" $file'_lowpass_boost'$ext -y
-        
+            
         #Gerando mapa para remoção de ruidos a partir da leitura dos picos na saída do Extension EBU R128
         ffmpeg -i $file'_lowpass_boost'$ext  -hide_banner -nostats -filter_complex ebur128="peak=+true" -f null - 2> $file'_ebur128.txt' | Format -y
 
@@ -77,20 +72,20 @@ function SpikeRemove {
         
         #Listando as linhas do arquivo
         $Extension=$Text | ForEach-Object{$_ -replace "t:","|"} `
-                      | ForEach-Object{$_ -replace "TARGE",""} `
-                      | ForEach-Object{$_ -replace "dBFS",""} `
-                      | ForEach-Object{$_ -replace "FTPK:","|"} `
-                      | ForEach-Object{$_ -replace "TPK:","|"} `
-                      | ForEach-Object{$_ -replace "LUFS",""} `
-                      | ForEach-Object{$_ -replace "LU",""} `
-                      | ForEach-Object{$_ -replace "M:","|"} `
-                      | ForEach-Object{$_ -replace "I:","|"} `
-                      | ForEach-Object{$_ -replace "S:","|"} `
-                      | ForEach-Object{$_ -replace "LRA:","|"} `
-                      | ForEach-Object{$_ -replace "-inf","-80"} `
-                      | ForEach-Object{$_ -replace "\s+",""} `
-                      | Where-Object { $_ -notmatch "size"} `
-                      | Where-Object { $_ -notmatch "Summary"} `
+        | ForEach-Object{$_ -replace "TARGE",""} `
+        | ForEach-Object{$_ -replace "dBFS",""} `
+        | ForEach-Object{$_ -replace "FTPK:","|"} `
+        | ForEach-Object{$_ -replace "TPK:","|"} `
+        | ForEach-Object{$_ -replace "LUFS",""} `
+        | ForEach-Object{$_ -replace "LU",""} `
+        | ForEach-Object{$_ -replace "M:","|"} `
+        | ForEach-Object{$_ -replace "I:","|"} `
+        | ForEach-Object{$_ -replace "S:","|"} `
+        | ForEach-Object{$_ -replace "LRA:","|"} `
+        | ForEach-Object{$_ -replace "-inf","-80"} `
+        | ForEach-Object{$_ -replace "\s+",""} `
+        | Where-Object { $_ -notmatch "size"} `
+        | Where-Object { $_ -notmatch "Summary"} `
 
 
         ###compor linha de comando do ffmpeg para remoção do ruido considerando deltaT atualmente 0.1 segundos
@@ -98,24 +93,25 @@ function SpikeRemove {
         $aFTPK = @();
         $aTime = @();
         $i=0;
-
+        
         foreach ($element in $Extension) { 
-
-        $item = $element	
-        $item=$item.split('|');
-        if($item.count -eq 9){
-            $aTime += $item[1]
-            $aFTPK += $item[7]       
-            $i++        
+        
+            $item = $element	
+            $item=$item.split('|');
+            if($item.count -eq 9){
+                $aTime += $item[1]
+                $aFTPK += $item[7]       
+                $i++        
+            }
         }
-    }
         #Write-Host "Total de Elementos: $($i)."
 
         $Clip=""
         $p = 0
+
         For ($t=0; $t -lt $i - 0 ; $t++) {  
             if([double]$aFTPK[$t] -eq 0){
-
+            
                 #$tCenter = [double]$aTime[$t] 
                 $tLeft = [double]$aTime[$t] - 0.15
                 $tRight = [double]$aTime[$t] + 0.15
@@ -123,7 +119,7 @@ function SpikeRemove {
                 $p++
             }
         }
-
+            
         # Write-Host "------------------------------------------------------------"
         # Write-Host "Foram encotrados: $($p) picos."
         # $temp = $clip
@@ -134,19 +130,19 @@ function SpikeRemove {
         #     Write-Host "Pico $($i+1): $($temp[$i])"
         # }
         #  Write-Host "------------------------------------------------------------" 
-
+        
         if(-Not ($clip -eq "")) {
             #Filtrar arquivo gerado removendo os picos de volume como saida final arquivo de audio
             #ffmpeg -i $in -af "$($clip.Replace("0vo","0,vo"))" $out'_spikeless'$ext -y
             ffmpeg -i $in -af "$($clip.Replace("0vo","0,vo"))" $out -y
         }
-        
+            
         else {
-
-
-        }
         
+        }
+            
         #Apaga os arquivos temporários gerados
+        
         
         Remove-Item ($file + '_lowpass' + $ext)
         Remove-Item ($file + '_lowpass_boost' + $ext)
@@ -159,6 +155,8 @@ function SpikeRemove {
 function EnhanceAudio {
 
     param($in,$out)
+
+    Write-Host "Normalizando audio..."
 
     if(Test-Path -Path $in -PathType Leaf){
         $file = (Get-Item $in).DirectoryName + '\' + (Get-Item $in).Basename
@@ -177,10 +175,10 @@ function EnhanceAudio {
         #Listando as linhas do arquivo
         #Aplicando substituições para leitura do arquivo de texto
         $Extension=$Text | ForEach-Object{$_ -replace "max_volume:","|Max|"} `
-                      | ForEach-Object{$_ -replace "dB",""} `
-                      | ForEach-Object{$_ -replace "\s+",""} `
-                      | Where-Object { $_ -notmatch "size"} `
-                      | Where-Object { $_ -notmatch "Summary"} `
+        | ForEach-Object{$_ -replace "dB",""} `
+        | ForEach-Object{$_ -replace "\s+",""} `
+        | Where-Object { $_ -notmatch "size"} `
+        | Where-Object { $_ -notmatch "Summary"} `
 
 
 
@@ -207,6 +205,7 @@ function EnhanceAudio {
 function SplitAudio {
 
     param($in,$out)
+    Write-Host 'Fatiando audio...'
 
     if(Test-Path -Path $in -PathType Leaf){
         $dir = (Get-Item $in).DirectoryName
@@ -281,8 +280,11 @@ function SplitAudio {
         #     New-Item $dir -Name $newfolder -ItemType "directory"
         # }
         $newfolder = Split-Path $out -Leaf
+
+        $out2 = $out.Replace($newfolder,'')
+
         If(!(Test-Path $out)) {
-            New-Item $dir -Name $newfolder -ItemType "directory"
+            New-Item $out2 -Name $newfolder -ItemType "directory"
         }
 
         # Divide os arquivos com base nos vetores mapeados
@@ -290,7 +292,7 @@ function SplitAudio {
             
             $Duration = [double]$aEnd[$i]-[double]$aStart[$i]
             # $name = $dir + '\' + $newfolder + '\' + $base + '_' + [string]$aStart[$i] + '_' + [string]$aEnd[$i] + $ext
-            $name = $out + $base + '_' + [string]$aStart[$i] + '_' + [string]$aEnd[$i] + $ext
+            $name = $out + '\' + $base + '#' + [string]$aStart[$i] + '#' + [string]$aEnd[$i] + $ext
             ffmpeg -ss $aStart[$i] -i $in -t $Duration -c copy $name -y
 
         }
@@ -299,85 +301,119 @@ function SplitAudio {
     }
 }
 
-function ConverteVideo {
-    param($arquivo, $arquivodestino, $Extension) # primeira linha
-
-    Write-Host "Essse comando ter� como sa�da um arquivo de video mpeg2 compat�vel com a maioria dos players web extra�do diretamente do arquivo .TS"
-    if ($arquivo -and $arquivodestino){
-
-        #    ffmpeg -i $arquivo -c:v libx264 -c:a aac -ac 1 -b:a 96k -af "afftdn,adeclip,adeclick=b=5,acompressor" -movflags +faststart -vf "yadif=1,scale=-2:720" $arquivodestino'.mp4'
-        ffmpeg -i $arquivo -c:v libx264 -ac 1 -movflags +faststart -vf "yadif=1,scale=-2:720" $arquivodestino'.mp4'
-
-
-
-    } else {
-        Write-Host "Arquivo n�o informado ou destino n�o informado Por favor execute:"
-        Write-Host "----------------------------------------------"
-        Write-Host "./converterParaMPEG.ps1 <nome do arquivo de video .TS> <caminho e nome do arquivo destino com extens�o .mp4>"
+function FMVVideo {
+    param($In, $Out, $Overwrite)
+    
+    #Verifica se Arquivo já existe, caso sim não reprocessa
+    if(Test-Path -Path $Out'.mp4') {
+        Write-Host "Arquivo existente"
     }
+    else {
+
+        Write-Host "Extraindo video..."
+   
+        if ($In -and $Out){
+
+            ffmpeg -i $In -c:v libx264 -ac 1 -movflags +faststart -vf "yadif=1,scale=-2:720" $Out'.mp4'
+        }
+        else {
+
+            Write-Host "Arquivo n�o informado ou destino n�o informado Por favor execute:"
+            Write-Host "----------------------------------------------"
+            Write-Host "./converterParaMPEG.ps1 <nome do arquivo de video .TS> <caminho e nome do arquivo destino com extens�o .mp4>"
+        }
+    }
+}
+
+function FMVAudio {
+    param($in,$out)
+    
+    $ext = (Get-Item $in).Extension
+    $temp = $out.Substring(0,$out.Length - $ext.Length) + '_mono'
+
+    ExtractAudio $in $temp
+
+    $temp = $temp + '.wav'
+    $temp2 = $temp + '_spikeless.wav'
+    
+    SpikeRemove $temp $temp2
+    Remove-Item $temp
+
+    EnhanceAudio $temp2 $($out + '.wav')
+    Remove-Item $temp2
+
+    Write-Host 'Concluido'
+    
+}
+
+function FMVData {
+    param($in,$out)
+    
+}
+
+function FMVSTT {
+    param($in,$out)
+    
+    $ext = (Get-Item $in).Extension
+    $temp = $out.Substring(0,$out.Length - $ext.Length) 
+
+    ExtractAudio $in $temp
+
+    $temp = $temp + '.wav'
+
+    SplitAudio $temp ($out + '_mono')
+    
+    Remove-Item $temp
+    
+    $Entrada = $out + '_mono'
+    $Sobre = '*.wav'
+    $Saida = $out
+   
+    Iterator $Entrada $Sobre -Call $function:FMVAudio $Saida
+
+    Remove-Item -LiteralPath $Entrada -Force -Recurse
+
+    Get-ChildItem -Recurse -Path $Saida -File -Include *.wav | ForEach-Object -Process {
+        $NewName = ($_.BaseName).Replace('.wav','')
+        Rename-Item -Path $_.FullName -NewName ($NewName + '.wav')
+    }
+
+
+    
 }
 
 function Iterator {
     param($Inlet,$Extension,[scriptblock]$Call,$Outlet)
-
+    
     $Origem = Convert-Path $Inlet
-    #Mede o tempo da execução da varredura
-    Measure-Command {
-        #verifica se já existe. Se não, cria
-        if(-Not (Test-Path -Path $Outlet)) {
+    
+    #verifica se já existe. Se não, cria
+    if(-Not (Test-Path -Path $Outlet)) {
             mkdir $Outlet
+    }
+        
+    #itera a lista de arquivos na pasta e cria pasta se arquivo atender ao critério de tipo
+    Get-ChildItem -Recurse -Path $Inlet -File -Include $Extension | ForEach-Object -Process {
+            
+        Write-Host "------------------------------------------------------------------------------------------------------"
+        Write-Host "Arquivo a ser processado: $($_.Name)"
+        $sub = ($_.FullName).Replace($Origem,'')
+        $dir = ($_.DirectoryName).Replace($Origem,'')
+        $destinovideo = $Outlet + $sub
+        
+        $dir = $Outlet + $dir + '\' 
+        
+        #verifica se já existe. Se não, cria
+        if(-Not (Test-Path -Path $dir)) {
+            Write-Host "Criando pasta: $($dir)"
+            New-Item -ItemType directory -Path $dir
+        }
+        else {
+            Write-Host "Pasta $($dir) ja criada"
         }
 
-        #itera a lista de diretórios na pasta
-        # Get-ChildItem -Recurse -Path $Inlet\ | Where-Object {$_.PSIscontainer} | ForEach-Object -Process {
-            
-        #     #concatena o caminho completo para o destino        
-        #     $sub = ($_.FullName).Replace($Origem,'\')
-        #     $video = $Outlet+$sub
-
-        #     #verifica se já existe. Se não, cria
-        #     if(-Not (Test-Path -Path $video)) {
-        #         "Criando pasta: " + $_.Name
-        #         New-Item -ItemType directory -Path $video
-        #     }
-        #     else {
-        #         write-host("Pasta " + $_.Name + " já criada.")
-        #     }
-
-        # }
-
-        
-        #itera a lista de arquivos na pasta e cria pasta se arquivo atender ao critério de tipo
-        Get-ChildItem -Recurse -Path $Inlet -File -Include $Extension | ForEach-Object -Process {
-            
-            "Arquivo a ser processado: " + $_.Name
-            $sub = ($_.FullName).Replace($Origem,'')
-            $dir = ($_.DirectoryName).Replace($Origem,'')
-            $destinovideo = $Outlet + $sub
-
-            $dir = $Outlet + $dir + '\' 
-            
-            #verifica se já existe. Se não, cria
-            if(-Not (Test-Path -Path $dir)) {
-                "Criando pasta: " + $_.Name
-                New-Item -ItemType directory -Path $dir
-            }
-            else {
-                write-host("Pasta " + $_.Name + " já criada.")
-            }
-            
-            #Verifica se Arquivo já existe, caso sim não reprocessa
-            if(Test-Path -Path $destinovideo'.mp4') {
-                "Arquivo "+ $destinovideo + " já existente! Nada a ser feito."
-            }
-            else {
-                $arquivoorigem=$_.FullName;
-                $arquivodestino=$destinovideo;
-                "Processa bin a partir de: " +$arquivoorigem +  " DESTINO: " + $arquivodestino
-                
-                $Call.Invoke($arquivoorigem, $arquivodestino)
-
-            }
-        }	                        
-    }
+        $arquivoorigem=$_.FullName;
+        $arquivodestino=$destinovideo;
+        $Call.Invoke($arquivoorigem, $arquivodestino)
+    }	                        
 }
--- sandbox.ps1
+++ sandbox.ps1
@@ -1,11 +1,46 @@
-$Entrada = '.\Data'
-$Sobre = ('*.ts')
-$Saida = '.\Output\TS_Audio'
 
-Iterator $Entrada $Sobre -Call $function:ExtractAudio $Saida
+. ".\library.ps1"
 
-# $Entrada = '.\Output\TS_Audio'
-# $Sobre = ('*.wav')
-# $Saida = '.\Output\TS_Spikeless'
 
-# Iterator $Entrada $Sobre -Call $function:SpikeRemove $Saida
+#Mede o tempo da execução da varredura
+Measure-Command {
+
+    $FMVVideo = 0
+    $FMVAudio = 0
+    $FMVSTT   = 1
+    $FMVData  = 0
+    
+    # $Entrada = '.\Data'
+    
+    $Entrada = '.\Data'
+    $Sobre = '*.ts'
+
+    if($FMVVideo) {
+
+        $Saida = '.\Output\TS_Video'
+        Iterator $Entrada $Sobre -Call $function:ExtractAudio $Saida
+    
+    }
+    
+    if($FMVAudio) {
+        
+        $Saida = '.\Output\TS_Audio'      
+        Iterator $Entrada $Sobre -Call $function:FMVAudio $Saida
+
+    }
+
+    if($FMVSTT) {
+
+        $Saida = '.\Output\TS_Test2'
+        Iterator $Entrada $Sobre -Call $function:FMVSTT $Saida
+        
+    }
+    
+    if($FMVSTT) {
+    
+        $Saida = '.\Output\TS_Dados'
+       
+        Iterator $Entrada $Sobre -Call $function:FMVData $Saida
+
+    }
+}
